3d0829f439d6372b0a9653a5e5e8b91bff5af14f
Hari <Khariprasath351@gmail.com>
2022-05-18 09:35:55 +0530

    version created for tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Icon from './component/icon'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+let itemArray=new Array(9).fill("empty")
+
+ const App=(props) => {
+const [isCross,setIsCross]=useState(false);
+const [winMessage,setWinMessage]=useState('')
+
+
+const reloadGame = () => {
+  setIsCross(false);
+  setWinMessage("");
+  itemArray.fill("empty", 0, 9);
+};
+
+const changeitem = itemindex => {
+  if(winMessage){
+    return toast.success(winMessage, {
+      theme: "colored"
+    })
+  }
+
+   
+
+  const checkWinMess = () =>{
+    if (
+      itemArray[0] === itemArray[1] &&
+      itemArray[0] === itemArray[2] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[3] !== "empty" &&
+      itemArray[3] === itemArray[4] &&
+      itemArray[4] === itemArray[5]
+    ) {
+      setWinMessage(`${itemArray[3]} won`);
+    } else if (
+      itemArray[6] !== "empty" &&
+      itemArray[6] === itemArray[7] &&
+      itemArray[7] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[6]} won`);
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[3] &&
+      itemArray[3] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[1] !== "empty" &&
+      itemArray[1] === itemArray[4] &&
+      itemArray[4] === itemArray[7]
+    ) {
+      setWinMessage(`${itemArray[1]} won`);
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[5] &&
+      itemArray[5] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[2]} won`);
+    } else if (
+      itemArray[0] !== "empty" &&
+      itemArray[0] === itemArray[4] &&
+      itemArray[4] === itemArray[8]
+    ) {
+      setWinMessage(`${itemArray[0]} won`);
+    } else if (
+      itemArray[2] !== "empty" &&
+      itemArray[2] === itemArray[4] &&
+      itemArray[4] === itemArray[6]
+    ) {
+      setWinMessage(`${itemArray[2]} won`);
+    }else if 
+      (itemArray[0] !== "empty"&&
+      itemArray[1] !== "empty"&&
+      itemArray[2] !== "empty"&&
+      itemArray[3] !== "empty"&&
+      itemArray[4] !== "empty"&&
+      itemArray[5] !== "empty"&&
+      itemArray[6] !== "empty"&&
+      itemArray[7] !== "empty"&&
+      itemArray[8] !== "empty")
+      
+    {
+      setWinMessage(`Match Draw`);
+    }
+  
+  }
+
+
+  if(itemArray[itemindex] == 'empty'){
+    itemArray[itemindex]= isCross ? 'cross':'circle' ;
+    setIsCross(!isCross);
+  }else{
+    return toast.error("Lorem ipsum dolor", {
+      theme: "colored"
+    })
+    ;
+  }
+
+
+  checkWinMess();
 }
+
+
+
+  return (
+    <>
+     <Container className='p-5'>
+     <ToastContainer position="top-center" />
+   <div className='grid'>
+     {
+       itemArray.map(( item, index) => 
+         <Card color='danger' onClick={() =>changeitem(index)}>
+           <CardBody className='box'>
+            <Icon name= {item} />
+           </CardBody>
+         </Card>
+       )
+     }
+   </div>
+ 
+   <Button color="success"  block onClick={reloadGame}>Reload the game
+   </Button>
+      
+     
+     </Container>
+
+    </>
+  );
+};
+
 
 export default App;
